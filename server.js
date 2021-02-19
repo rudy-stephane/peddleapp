899af1d043aa9bb046d3bb8f47c08350aeae6a97
rudy-stephane <tekamfossi@gmail.com>
2021-02-19 20:47:03 +0100

    add new origin

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(cors());
 //CORS Middleware
 app.use(function (req, res, next) {
 //Enabling CORS
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://www.linkedin.com");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
   next();
