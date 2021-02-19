edc23e984ca650b356ec71df146da9000b53ffb7
rudy-stephane <tekamfossi@gmail.com>
2021-02-19 21:00:49 +0100

    add new origin  cors

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-app.use(cors());
+//app.use(cors());
 
 //CORS Middleware
 app.use(function (req, res, next) {
