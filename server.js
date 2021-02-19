7dd00af3cdb75e4926478f19edfa819c6fa5f3e5
rudy-stephane <tekamfossi@gmail.com>
2021-02-19 20:30:55 +0100

    initialize cors()

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ const cors = require('cors');
 
 app.use(cors());
 
+//CORS Middleware
+app.use(function (req, res, next) {
+//Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+  next();
+});
+
 app.use(express.static('./dist/peddle'));
 
 app.get('/*', (req, res) =>
